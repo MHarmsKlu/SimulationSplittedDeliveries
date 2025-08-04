82f0f6504af682e885ccf0df664ff738428e62aa
Jostafarr <jost.goette@web.de>
2025-08-04 10:08:48 +0200

    update todos

--- simulation/experiment.py
+++ simulation/experiment.py
@@ -6,21 +6,28 @@ from math import sin,log
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import tqdm
+import shutil
+import os
 
-def initialize_sim(kpi, war_config, sim_config, test_param, test_value):
-    if test_param in war_config.keys():
-        war_config[test_param]= test_value
+def initialize_sim(kpi, sku_config, sim_config, test_param, test_value):
+    if test_param in sku_config.keys():
+        sku_config[test_param]= test_value
     elif test_param in sim_config.keys():
         sim_config[test_param] = test_value
     else: 
         print(f"invalid test_param {test_param}")
     
-    war_config['kpi'] = kpi
-    sim_config["warehouse"] = Warehouse(war_config)
+    sku_config['kpi'] = kpi
+    sim_config["warehouse"] = Warehouse([sku_config])
     return Simulation( config=sim_config)
 
-def experiment(KPIs,war_config, sim_config,test_param,init_value, step_size, max_value, target_params):
-    
+def experiment(KPIs,sku_config, sim_config,test_param,init_value, step_size, max_value, target_params):
+    try:
+        shutil.rmtree(sim_config['output'])
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+    os.makedirs(sim_config['output'])
+
     results = {}
     for target_param in target_params:
         results[target_param] = {}
@@ -29,7 +36,7 @@ def experiment(KPIs,war_config, sim_config,test_param,init_value, step_size, max
         for target_param in target_params:
             results[target_param][kpi] = []
         for i in tqdm(range(init_value,max_value,step_size)):
-            sim = initialize_sim(kpi,war_config,sim_config,test_param,i)
+            sim = initialize_sim(kpi,sku_config,sim_config,test_param,i)
 
             sim.run()
             sim_results = sim.evaluate_globally()
@@ -60,14 +67,15 @@ delivery_functions = [
     lambda x: x**2,
     lambda x: -log(x)
 ]
-
-war_config = {
+sku_config = {
+    'id' : 0,
     'rop' : 500,
     'eoq' : 0,
     'z_score': 1.65,
     'order_base_cost' : 60,
     'holding_cost' : 1 , 
     'inventory' : 500,
+    'verbose': False
 }
 
 sim_config =  {
@@ -76,10 +84,11 @@ sim_config =  {
     'seed': 1,  
     'mean_daily_demand' : 50,
     'std_daily_demand' : 1,
-    'delivery_func' : lambda x: 100,
+    'delivery_func' : [lambda x: 100],
     'delivery_split_centre' : 0,
-    'delivery_split_std' : 1}
+    'delivery_split_std' : 1,
+    'output': "experiment"}
 
 target_params = ["service_level", "total_inventory_on_hand"]
 
-experiment(KPIs,war_config,sim_config, "delivery_split_centre", 1,1, 10, target_params  )
+experiment(KPIs,sku_config,sim_config, "delivery_split_centre", 1,1, 10, target_params  )
